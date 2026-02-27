719d9b4eee87eef01ed3a5f50392e4997d8d79fd
ROCGMC <a0960730188@gmail.com>
2026-02-27 18:49:23 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'zt-game-v3';
+const CACHE_NAME = 'zt-game-v4';
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(['./', './index.html'])));
 });
