4628e9f3f129c1cc1533b6b0c003d2538772c39b
ROCGMC <a0960730188@gmail.com>
2026-02-27 18:27:34 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'zt-guess-v1';
+const assets = ['./', './index.html', 'https://unpkg.com/vue@3/dist/vue.global.js', 'https://unpkg.com/peerjs@1.5.2/dist/peerjs.min.js'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
